b9ec5091692f05f4d737b80714b3e3e55ff14de6
Benjamin Semah <benjaminsemah@gmail.com>
2022-06-11 16:25:24 +0000

    Connect react frontend to rails api

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,15 +1,8 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchGreetingApi } from '../store/greetings';
+import React from "react"
+const Home = () => {
+  return (
+    <h1>HOME</h1>
+  );
+}
 
-const Greeting = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchGreetingApi());
-  }, []);
-  const message = useSelector(({ msgReducer }) => msgReducer);
-
-  return <React.Fragment>Greeting: {message}</React.Fragment>;
-};
-
-export default Greeting;
+export default Home
