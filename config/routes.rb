eed69fc725c423be94f2dd9bcfb03c86814459f8
Benjamin Semah <benjaminsemah@gmail.com>
2022-06-11 15:04:16 +0000

    Configure route routes and add components

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: 'json' } do
+    get "message", to: "messages#index"
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "*page", to: "static#index", constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+  root "static#index"
 end
